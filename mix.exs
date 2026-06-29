eecdf75a9fe39d59258a056e18b57f79e114361b
Oliver Severin Mulelid-Tynes <oliver@omt.tech>
2026-06-29 14:17:14 +0200

    chore: leave version to release tooling

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule AshLua.MixProject do
   An Ash extension for embedding Lua scripts with a consistent actor, tenant, and context.
   """
 
-  @version "0.2.0-rc.0"
+  @version "0.1.6"
 
   @source_url "https://github.com/ash-project/ash_lua"
 
